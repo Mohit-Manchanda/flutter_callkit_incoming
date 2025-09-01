ae241556fc51ba826f61e2abe9174758f9fa14db
Mohit-Manchanda <marskindustrieslimited@gmail.com>
2025-09-01 18:07:30 +0530

    mic_action

--- android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
+++ android/src/main/kotlin/com/hiennv/flutter_callkit_incoming/CallkitNotificationManager.kt
@@ -661,7 +661,7 @@ class CallkitNotificationManager(
                     context.getString(R.string.text_mute), // or "Mute" directly
                     togglePending
                 ).build()
-                notificationOngoingBuilder?.addAction(muteAction) m
+                notificationOngoingBuilder?.addAction(muteAction)
 
                 val isShowCallID =
                     data.getBoolean(CallkitConstants.EXTRA_CALLKIT_IS_SHOW_CALL_ID, false)
